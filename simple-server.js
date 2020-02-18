52c06e427b07435578e8a7d5d4f65e410061db8d
Austin Stone (RIT Student) <axs6207@ad.rit.edu>
2020-02-18 14:51:51 -0500

    done

--- simple-server.js
+++ simple-server.js
@@ -15,6 +15,52 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 let jokesArray;
 
+const handlerNotFound = (request,response) => {
+	const statusCode = 404;
+	response.writeHead(statusCode, {'Content-Type' : 'text/html'});
+	response.write("<html><body><h1>404 not found</h1></body></html>");
+	response.end();
+}
+
+const handlerGetAllJokes = (request,response) => {
+	const statusCode = 200;
+	response.writeHead(statusCode, {'Content-Type' : 'application/json'});
+	response.write(JSON.stringify(jokesArray));
+	response.end();
+}
+
+const handlerGetRandomJoke = (request,response) => {
+	const statusCode = 200;
+	response.writeHead(statusCode, {'Content-Type' : 'application/json'});
+	let joke = jokesArray[Math.floor(Math.random() * Math.floor(jokesArray.length))];
+	response.write(JSON.stringify(joke));
+	response.end();
+}
+
+const urlStruct = {
+	'GET': {
+		'/getAllJokes' : handlerGetAllJokes,
+		'/getRandomJoke' : handlerGetRandomJoke
+	},
+	'POST': {},
+	'HEAD': {},
+	notFound: handlerNotFound
+}
+
+const onRequest = (request,response) => {
+	const parsedUrl = url.parse(request.url);
+	const pathname = parsedUrl.pathname;
+	const httpMethod = request.method;
+	const params = query.parse(parsedUrl.query);
+	
+	if(urlStruct[httpMethod][pathname]){
+		urlStruct[httpMethod][pathname](request,response);
+	}
+	else{
+		urlStruct.notFound(request,response);
+	}
+}
+
 // request handlers
 
 // urlStruct
@@ -24,11 +70,13 @@ let jokesArray;
 
 let init = () => {
 	// load jokes
-	const jokes = fs.readFileSync(`${__dirname}/jokes.json`);
+	const jokesFile = fs.readFileSync(`${__dirname}/jokes.json`);
+	
+	jokesArray = JSON.parse(jokesFile).jokes;
 	
 	// create server
 	http.createServer(onRequest).listen(port);
 	console.log(`Listening on 127.0.0.1: ${port}`);
 }
 
-//init();
+init();
